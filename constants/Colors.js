37956766d69c55b4492ae3d0102ac691bf42b6db
thylnk <thylengockhanh@gmail.com>
2022-05-27 22:50:09 +0700

    init codebase

---
+++ constants/Colors.js
@@ -0,0 +1,30 @@
+export const colors = {
+  tintColorLight: '#2f95dc',
+  tintColorDark: '#fff',
+  black: '#000',
+  primaryColor: '#200378',
+  secondaryColor: '#5B31B2',
+  lightPurple: '#8F73C5',
+  purple: '#ABA4D4',
+  gray: '#E2E3FC',
+  lightDark: '#322F3F',
+  white: '#fff',
+}
+
+// export default {
+//   light: {
+//     text: '#000',
+//     textWhite: white,
+//     background: '#E2E3FC',
+//     tint: tintColorLight,
+//     tabIconDefault: '#ccc',
+//     tabIconSelected: tintColorLight,
+//   },
+//   dark: {
+//     text: '#fff',
+//     background: '#000',
+//     tint: tintColorDark,
+//     tabIconDefault: '#ccc',
+//     tabIconSelected: tintColorDark,
+//   },
+// };
