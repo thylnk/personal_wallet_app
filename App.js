9f5b5d3b4557268fd6861f7541ec3cf95cc8f413
thylnk <thylengockhanh@gmail.com>
2022-06-23 02:38:58 +0700

    feat(PW-25): config splashscreen

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
 import { NavigationContainer } from '@react-navigation/native';
-import AppLoading from 'expo-app-loading';
-import React, { useState } from 'react';
+// import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useState, useEffect, useCallback } from 'react';
 import AppNavigation from '~navigation/AppNavigation';
 import useFonts from '~shared/hook/useFonts';
 
 export default function App() {
 
-  const [isReady, SetIsReady] = useState(false);
+  const [isReady, setIsReady] = useState(false);
 
-  const loadFonts = async () => {
-    await useFonts();
+  const onLayoutRootView = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
   };
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await useFonts();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }, []);
+
+
   if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => SetIsReady(true)}
-        onError={() => { }}
-      />
-    );
+    return null;
   }
+
+
   return (
     <NavigationContainer>
       <AppNavigation />
